b9b9bba01d0f73f811c0e3eddf80e229d0246c1d
Patrik Oliveira <patrikro.oliveira@gmail.com>
2022-05-13 15:16:55 -0300

    adiciona pattern notification como acumulador de errors para customers

---
+++ src/domain/@shared/notification/notification.error.ts
@@ -0,0 +1,9 @@
+import { NotificationErrorProps } from "./notification";
+
+export class NotificationError extends Error {
+  constructor(public erros: NotificationErrorProps[]) {
+    super(
+      erros.map((error) =>  `${error.context}: ${error.message}`).join(",")
+    );
+  }
+}
