34e88aa70ae64cba82d26a0dbc8ea4530b2a0f21
Patrik Oliveira <patrikro.oliveira@gmail.com>
2022-05-13 17:09:01 -0300

    adiciona o yup como validador para product

--- src/domain/product/entity/product.ts
+++ src/domain/product/entity/product.ts
@@ -1,9 +1,9 @@
 import { Entity } from "../../@shared/entity/entity.abstract";
 import { NotificationError } from "../../@shared/notification/notification.error";
+import { ProductValidatorFactory } from "../factory/product.validator.factory";
 import ProductInterface from "./product.interface";
 
 export default class Product extends Entity implements ProductInterface {
-  // private _id: string;
   private _name: string;
   private _price: number;
 
@@ -37,28 +37,13 @@ export default class Product extends Entity implements ProductInterface {
     this.validate();
   }
 
-  validate(): boolean {
-    if (this._id.length === 0) {
-      this.notification.addError({
-        context: "product",
-        message: "Id is required",
-      });
-    }
-    if (this._name.length === 0) {
-      this.notification.addError({
-        context: "product",
-        message: "Name is required",
-      });
-    }
-    if (this._price < 0) {
-      this.notification.addError({
-        context: "product",
-        message: "Price must be greater than zero",
-      });
-    }
+  validate() {
+    ProductValidatorFactory
+      .create()
+      .validate(this);
+
     if (this.notification.hasErrors()) {
       throw new NotificationError(this.notification.getErrors());
     }
-    return true;
   }
 }
